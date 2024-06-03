90439418e6f2e3aecbfa8802c15b50e2935f200a
Itachi1905 <162597171+Itachi1905@users.noreply.github.com>
2024-06-03 17:12:13 +0530

    Organizing

--- backend/exercise1/index.js
+++ backend/exercise1/index.js
@@ -1,41 +1,24 @@
-// You have to write a Node.js program to clear clutter inside of a directory and organize the contents of that directory into different folders
+import fs from "fs/promises";
+import fsn from "fs";
+import path from "path";
 
-// for example, these files become:
+const basepath = "C:\\Users\\Itachi\\Documents\\Visual Studio Code\\web_dev\\backend\\exercise1";
 
-// 1. name.jpg
-// 2. name.png
-// 3. this.pdf 
-// 4. harry.zip
-// 5. Rohan.zip
-// 6. cat.jpg 
-// 7. harry.pdf
-
-// this: 
-// jpg/name.jpg, jpg/cat.jpg 
-// png/name.png 
-// pdf/this.pdf pdf/harry.pdf
-// zip/harry.zip zip/Rohan.zip
-
-import fs from "fs/promises"
-import fsn from "fs"
-import path from "path"
-
-const basepath = "C:\\Users\\Itachi\\Documents\\Visual Studio Code\\web_dev\\backend\\exercise1"
-
-let files = await fs.readdir(basepath)
+let files = await fs.readdir(basepath);
 
 for (const item of files) {
-    console.log("running for ", item)
-    let ext = item.split(".")[item.split(".").length - 1]
+    console.log("running for ", item);
+    let ext = item.split(".")[item.split(".").length - 1];
     if (ext != "js" && ext != "json" && item.split(".").length > 1) {
-        if (fsn.existsSync(path.join(basepath, ext))) {
-            // Move the file to this directory if its not a js or json file
-            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
-        }
-        else {
-            fs.mkdir(ext)
-            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        const targetDir = path.join(basepath, ext);
+
+        if (fsn.existsSync(targetDir)) {
+            // Move the file to this directory if it already exists and it's not a js or json file
+            await fs.rename(path.join(basepath, item), path.join(targetDir, item));
+        } else {
+            // Create the directory and then move the file
+            await fs.mkdir(targetDir);
+            await fs.rename(path.join(basepath, item), path.join(targetDir, item));
         }
     }
-
 }
--- frontend/Vowels.js
+++
@@ -1,8 +0,0 @@
-let str = "Alpha Coder"
-let x = 0;
-let l = str.length
-let arr = ['a','e','i','o','u','A','E','I','O','U']
-while(l--) {
-    if( arr.find( element => element == str[l] ) ) x++; 
-}
-console.log(x)
